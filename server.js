30e56529cb0b8cb7fc4ab8f2b5f6fedd7d991102
jub <rubikscubescramble@gmail.com>
2026-01-16 11:56:16 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import './src/index.js'; // change this to the path of your bot's main file
+
+const app = express();
+
+// Minimal endpoint for uptime monitoring
+app.get("/", (req, res) => {
+  res.send("ButlerMC is alive!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
